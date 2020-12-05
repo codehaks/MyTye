3fe381a53f17ab11bc9fc8fc02067e27bd2c62cb
Hakim Ghods <ghods.hakim@hotmail.com>
2020-12-05 11:26:52 +0330

    tye

--- MyApp/Pages/Index.cshtml.cs
+++ MyApp/Pages/Index.cshtml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,24 +8,6 @@ using MyApp.Models;
 
 namespace MyApp
 {
-    public class WeatherClient
-    {
-        private readonly HttpClient _client;
-
-        public WeatherClient(HttpClient client)
-        {
-            _client = client;
-        }
-
-        public async Task<ICollection<WeatherInfo>> GetWeatherInfo()
-        {
-            return await
-                (
-                  (await _client.GetAsync("https://localhost:5101/weatherforecast"))
-                  .Content
-                  .ReadFromJsonAsync<ICollection<WeatherInfo>>());
-        }
-    }
     public class IndexModel : PageModel
     {
         private readonly WeatherClient _weatherClient;
---
+++ MyApp/WeatherClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MyApp.Models;
+
+namespace MyApp
+{
+    public class WeatherClient
+    {
+        private readonly HttpClient _client;
+
+        public WeatherClient(HttpClient client,IConfiguration configuration)
+        {
+            _client = client;
+            Configuration = configuration;
+
+            
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public async Task<ICollection<WeatherInfo>> GetWeatherInfo()
+        {
+            var weatherService = Configuration.GetServiceUri("weatherservice");
+
+            return await
+                (
+                  (await _client.GetAsync(weatherService+"weatherforecast"))
+                  .Content
+                  .ReadFromJsonAsync<ICollection<WeatherInfo>>());
+        }
+    }
+}
